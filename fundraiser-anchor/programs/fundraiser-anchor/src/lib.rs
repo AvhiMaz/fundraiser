f97dc090edc40bd47621b879cee7755750e733e2
AvhiMaz <avhimazumder5@outlook.com>
2025-04-15 17:08:57 +0530

    anchor: add states

--- fundraiser-anchor/programs/fundraiser-anchor/src/lib.rs
+++ fundraiser-anchor/programs/fundraiser-anchor/src/lib.rs
@@ -1,16 +1,12 @@
+#![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
+
+mod instructions;
+mod states;
 
 declare_id!("D9Du7gCDii6fUCBiuDqrcMLKQJR482BeTrMNPP7W936M");
 
 #[program]
 pub mod fundraiser_anchor {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
