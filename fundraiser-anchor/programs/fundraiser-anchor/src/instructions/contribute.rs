a0ec27c5c4f48baba645167c6af0a23ef6b3333d
AvhiMaz <avhimazumder5@outlook.com>
2025-04-15 19:53:01 +0530

    anchor: add comments and update logic

--- fundraiser-anchor/programs/fundraiser-anchor/src/instructions/contribute.rs
+++ fundraiser-anchor/programs/fundraiser-anchor/src/instructions/contribute.rs
@@ -10,11 +10,14 @@ use crate::states::{Contributor, Fundraiser};
 
 #[derive(Accounts)]
 pub struct Contribute<'info> {
+    // contributor
     #[account(mut)]
     pub contributor: Signer<'info>,
 
+    // token needed for fundraiser
     pub which_mint: Account<'info, Mint>,
 
+    // fundraiser pda
     #[account(
         mut,
         has_one= which_mint,
@@ -23,6 +26,7 @@ pub struct Contribute<'info> {
     )]
     pub fundraiser: Account<'info, Fundraiser>,
 
+    // contributor_acc
     #[account(
         init_if_needed,
         payer = contributor,
@@ -32,6 +36,7 @@ pub struct Contribute<'info> {
     )]
     pub contributor_acc: Account<'info, Contributor>,
 
+    //contributor_acc_ata which holds the token
     #[account(
         mut,
         associated_token::mint = which_mint,
@@ -39,12 +44,15 @@ pub struct Contribute<'info> {
     )]
     pub contributor_acc_ata: Account<'info, TokenAccount>,
 
+    // vault which hold token of fundraiser
     #[account(
         mut,
         associated_token::mint = fundraiser.which_mint,
         associated_token::authority = fundraiser
     )]
     pub vault: Account<'info, TokenAccount>,
+
+    // req programs
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
@@ -52,6 +60,7 @@ pub struct Contribute<'info> {
 
 impl<'info> Contribute<'info> {
     pub fn contribute(&mut self, amount: u64) -> Result<()> {
+        // transfer logic
         let cpi_program = self.token_program.to_account_info();
 
         let cpi_account = Transfer {
@@ -63,6 +72,9 @@ impl<'info> Contribute<'info> {
         let cpi_ctx = CpiContext::new(cpi_program, cpi_account);
 
         transfer(cpi_ctx, amount)?;
+
+        self.fundraiser.current_amount += amount;
+        self.contributor_acc.amount += amount;
 
         Ok(())
     }
