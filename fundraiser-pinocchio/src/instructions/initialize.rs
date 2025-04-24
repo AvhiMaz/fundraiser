f857d676503bb06cce23ee3030d25cbb92bc784f
AvhiMaz <avhimazumder5@outlook.com>
2025-04-24 21:08:38 +0530

    pinocchio: if applied this commit would be init instruction

--- fundraiser-pinocchio/src/instructions/initialize.rs
+++ fundraiser-pinocchio/src/instructions/initialize.rs
@@ -1,7 +1,21 @@
 use pinocchio::{
-    account_info::AccountInfo, program_error::ProgramError, pubkey::Pubkey, ProgramResult,
+    account_info::AccountInfo,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    sysvars::{rent::Rent, Sysvar},
+    ProgramResult,
 };
 use pinocchio_token::state::TokenAccount;
+
+use crate::states::load_ix_data;
+
+#[repr(C)]
+#[derive(Clone, Debug, Copy, PartialEq)]
+pub struct InitializeInstructionData {
+    pub amount: u64,
+    pub duration: u8,
+    pub bump: u8,
+}
 
 pub fn process_initialize(
     _program_id: &Pubkey,
@@ -29,6 +43,12 @@ pub fn process_initialize(
     let vault_acc = TokenAccount::from_account_info(vault)?;
 
     assert_eq!(vault_acc.owner(), fundraiser.key());
+
+    let rent = Rent::get()?;
+
+    let ix_data = unsafe {
+        load_ix_data::<InitializeInstructionData>(instruction_data)?;
+    };
 
     Ok(())
 }
